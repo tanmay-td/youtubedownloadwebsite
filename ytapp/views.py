218d4217678890bc58e44f119a782fb42d9f036e
tanmaytd <tanmaydeshmukhtd1@gmail.com>
2021-06-04 20:23:47 +0530

    changes in views.py

--- ytapp/views.py
+++ ytapp/views.py
@@ -13,6 +13,9 @@ def youtubevideo(request):
     qulity360pbt = False
     qulity480pbt =False
     qulity720pbt =False
+    title =''
+    url360 =''
+    url720=''
     
     if request.method =='POST':
         yturl =request.POST.get('url')
@@ -20,9 +23,9 @@ def youtubevideo(request):
         title = yt.title
         thumbnail = yt.thumbnail_url
         st =yt.streams.filter(progressive=True)
-        if st !=0:
+        if len(st) !=0:
                 qulity360p = yt.streams.filter(res="360p",progressive=True)
-                if qulity360p !=0:
+                if len(qulity360p) !=0:
                     print("360p Avalable")
                     print(qulity360p[0].url)
                     qulity360pbt =True
@@ -35,11 +38,14 @@ def youtubevideo(request):
                 #     qulity480pbt =True    
                 #     url480 =qulity480p[0].url
                 qulity720p = yt.streams.filter(res="720p",progressive=True)
-                if qulity720p !=0:
+                if len(qulity720p) !=0:
                     print("720p Avalable")
                     print(qulity720p[0].url)
                     qulity720pbt =True
                     url720 =qulity720p[0].url
+                    
+        else:
+            return HttpResponse("Link is not Valid")
         return render(request,'avalableformat.html',context={'qulity360pbt':qulity360pbt,'qulity480pbt':qulity480pbt,
                                                                  'qulity720pbt':qulity720pbt,'url360':url360,'url720':url720,
                                                                  'title':title,'thumbnail':thumbnail})
